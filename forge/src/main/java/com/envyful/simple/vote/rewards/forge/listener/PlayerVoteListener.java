66f01755427ef073c9218c5dcc21765580e1d58c
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-08-31 23:25:11 +0100

    Test vote command & vote party (#4)
    * Feat: added test vote command
    
    * build: version bump
    
    * feat(forge): add vote party
    
    * fix(forge): run server commands on main thread

--- forge/src/main/java/com/envyful/simple/vote/rewards/forge/listener/PlayerVoteListener.java
+++ forge/src/main/java/com/envyful/simple/vote/rewards/forge/listener/PlayerVoteListener.java
@@ -5,6 +5,7 @@ import com.envyful.api.forge.server.UtilForgeServer;
 import com.envyful.simple.vote.rewards.forge.SimpleVoteRewardsForge;
 import com.vexsoftware.votifier.sponge.event.VotifierEvent;
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraftforge.fml.common.FMLCommonHandler;
 import org.spongepowered.api.event.Listener;
 
 import java.util.concurrent.ThreadLocalRandom;
@@ -26,6 +27,23 @@ public class PlayerVoteListener {
         }
 
         voteRewards(player, this.mod);
+        this.mod.setVoteCounter(this.mod.getVoteCounter() + 1);
+
+        if (this.mod.getVoteCounter() < this.mod.getConfig().getVotePartyRequired()) {
+            return;
+        }
+
+        this.mod.setVoteCounter(0);
+
+        for (EntityPlayerMP entityPlayerMP : FMLCommonHandler.instance().getMinecraftServerInstance().getPlayerList().getPlayers()) {
+            for (String playerVotePartyCommand : this.mod.getConfig().getPlayerVotePartyCommands()) {
+                UtilForgeServer.executeCommand(playerVotePartyCommand.replace("%player%", entityPlayerMP.getName()));
+            }
+        }
+
+        for (String serverVotePartyCommand : this.mod.getConfig().getServerVotePartyCommands()) {
+            UtilForgeServer.executeCommand(serverVotePartyCommand);
+        }
     }
 
     public static void voteRewards(EntityPlayerMP player, SimpleVoteRewardsForge mod) {
